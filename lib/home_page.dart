aec09a4d60b9f9f14c4651a0c65c52570b55d583
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-01 21:38:48 +0100

    update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:crash_flutter/learn_flutter_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,8 +7,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:
-          ElevatedButton(onPressed: () {}, child: const Text('Learn Flutter')),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (BuildContext context) {
+              return const LearnFlutterPage();
+            }),
+          );
+        },
+        child: const Text('Learn Flutter'),
+      ),
     );
   }
 }
