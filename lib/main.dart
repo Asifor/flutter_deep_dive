a2b298e22a47b61a488dee08f45ce34d2b675664
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-04 05:46:32 +0100

    update profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
+import 'profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,13 +29,14 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [HomePage(), ProfilePage()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Deep Dive"),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             debugPrint('Floating Action Button');
